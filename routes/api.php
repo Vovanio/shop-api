e0671293d38135340ac9437e077258d16c76586a
whoami <skutelnikvova@gmail.com>
2022-05-26 14:20:56 +0300

    добавление аккаунта админа и мидлвара на проверку вошел ли пользователь

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('products', 'App\Http\Controllers\ProductsController@index');
-Route::post('products/add', 'App\Http\Controllers\ProductsController@store');
-Route::post('products/update', 'App\Http\Controllers\ProductsController@update');
-Route::post('products/delete', 'App\Http\Controllers\ProductsController@destroy');
 
-Route::post('email/send', 'App\Http\Controllers\MailController@SendMail');
+Route::middleware('auth.check:api')->group(function (){
+    Route::post('products/add', 'App\Http\Controllers\ProductsController@store');
+    Route::post('products/update', 'App\Http\Controllers\ProductsController@update');
+    Route::post('products/delete', 'App\Http\Controllers\ProductsController@destroy');
+    Route::post('email/send', 'App\Http\Controllers\MailController@SendMail');
+});
+
+Route::post('admin/login', 'App\Http\Controllers\AuthController@Login');
+
+
+
+
